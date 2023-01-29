93d72920bcff7537f7260ae1c994f6351d54801e
Yu0823 <55206903+Yu0823@users.noreply.github.com>
2023-01-29 16:21:29 -0500

    func: db

--- memcache/__init__.py
+++ memcache/__init__.py
@@ -4,10 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 pymysql.install_as_MySQLdb()
 
-global memcache
-
 webapp = Flask(__name__)
-memcache = {}
 
 class DBconfig(object):
     config_str = f'mysql://{DB_USER}:{DB_PSWD}@{IP_EC2}:{DB_PORT}/{DB}'
@@ -21,26 +18,22 @@ class DBconfig(object):
 webapp.config.from_object(DBconfig)
 db = SQLAlchemy(webapp)
 
-# class Role(db.Model):
-#     # 定义表名
-#     __tablename__ = 'roles'
-#     # 定义字段
-#     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-#     name = db.Column(db.String(64), unique=True)
-#     users = db.relationship('User',backref='role') # 反推与role关联的多个User模型对象
-#
-# class User(db.Model):
-#     # 定义表名
-#     __tablename__ = 'users'
-#     # 定义字段
-#     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
-#     name = db.Column(db.String(64), unique=True, index=True)
-#     email = db.Column(db.String(64),unique=True)
-#     pswd = db.Column(db.String(64))
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) # 设置外键
+from memcache import database
 
-with webapp.app_context():
-    db.create_all()
+# create tables
+# database.creat_tables()
+
+database.get_config()
+
+# config
+# capacity in MB
+CAPACITY = 100
+# 0 - Random; 1 - LRU
+POLICY = 0
+
+# key: (image, timestamp)
+global memcache
+memcache = {}
 
 from memcache import main
 
