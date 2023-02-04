29aea8f5a3c2ba3f13c792d8b19b8b8cf6b994cc
Jojo Duan <jiujiuduan@gmail.com>
2023-02-03 20:43:24 -0500

    add file and key uodate

--- web_front_end/__init__.py
+++ web_front_end/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-
+UPLOAD_FOLDER = '.'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "super secret key"
 
 from web_front_end import main
