a543c9e9c54e8f198e3b1c844514543cc9d280ce
Jojo Duan <jiujiuduan@gmail.com>
2023-02-20 22:59:54 -0500

    quick fix

--- web_front_end/find_a_image.py
+++ web_front_end/find_a_image.py
@@ -1,6 +1,6 @@
 from web_front_end import app
 from flask import Flask, flash, request, redirect, url_for,  render_template
-import requests
+#import requests
 
 @app.route('/find', methods=['GET', 'POST'])
 def find_image():
@@ -10,8 +10,8 @@ def find_image():
             flash('No key part')
             return render_template('show_image.html', value='please enter the key value')
         # has key, look into cache
-        r = requests.get("http://127.0.0.1:5002/api")
-        print(r.json())
+        #r = requests.get("http://127.0.0.1:5002/api")
+        #print(r.json())
         #if image in cache no found,
         #we look at the DB get the key and address of the file
         address = key_to_dir(key)
